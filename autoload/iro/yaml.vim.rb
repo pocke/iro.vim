85d8aa146d2506cc9623011269c17748865712ec
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-10-01 00:09:52 +0900

    Use traverser to highlight mapping key

--- autoload/iro/yaml.vim.rb
+++ autoload/iro/yaml.vim.rb
@@ -4,66 +4,55 @@ require 'json'
 
 module Iro
   module YAML
-    class Parser < ::Psych::Parser
-      def initialize(source)
-        @source = source
-        super(MarkHandler.new(self, source))
-      end
-
-      def parse
-        super(@source)
+    def self.traverse(node)
+      case node
+      when Psych::Nodes::Mapping
+        node.children.each_slice(2) do |key, value|
+          register_token 'Identifier', key
+          traverse(value)
+        end
+      when Psych::Nodes::Scalar
+        case @scanner.tokenize(node.value)
+        when String
+          register_token 'String', node
+        when TrueClass, FalseClass
+          register_token 'Boolean', node
+        when Integer
+          register_token 'Number', node
+        end
+      else
+        node.children&.each do |c|
+          traverse(c)
+        end
       end
     end
 
-    class MarkHandler < ::Psych::TreeBuilder
-      attr_reader :tokens
+    def self.register_token(group, node)
+      @tokens[group] ||=[]
 
-      def initialize(parser, source)
-        @parser = parser
-        @source = source.force_encoding(Encoding::UTF_8).split("\n")
-        @tokens = {}
-        @scanner = Psych::ScalarScanner.new(Psych::ClassLoader.new)
-        super()
-      end
-
-      def scalar(value, anchor, tag, plain, quoted, style)
-        super.tap do |node|
-          case @scanner.tokenize(value)
-          when String
-            register_token 'String', node
-          when TrueClass, FalseClass
-            register_token 'Boolean', node
-          when Integer
-            register_token 'Number', node
-          end
+      if node.start_line == node.end_line
+        size = @source[node.start_line][node.start_column..node.end_column].bytesize
+        @tokens[group] << [node.start_line+1, node.start_column+1, size]
+      else
+        @tokens[group] << [
+          node.start_line+1,
+          node.start_column+1,
+          @source[node.start_line].bytesize - node.start_column]
+        @source[(node.start_line+1)...node.end_line].each.with_index(1) do |line, idx|
+          @tokens[group] << [node.start_line+1+idx, 0, line.size]
         end
-      end
-
-      private
-
-      def register_token(group, node)
-        @tokens[group] ||=[]
-
-        if node.start_line == node.end_line
-          size = @source[node.start_line][node.start_column..node.end_column].bytesize
-          @tokens[group] << [node.start_line+1, node.start_column+1, size]
-        else
-          @tokens[group] << [
-            node.start_line+1,
-            node.start_column+1,
-            @source[node.start_line].bytesize - node.start_column]
-          @source[(node.start_line+1)...node.end_line].each.with_index(1) do |line, idx|
-            @tokens[group] << [node.start_line+1+idx, 0, line.size]
-          end
-          @tokens[group] << [node.end_line+1, 0, node.end_column]
-        end
+        @tokens[group] << [node.end_line+1, 0, node.end_column]
       end
     end
 
     def self.tokens(bufnr)
+      @tokens = {}
+      @scanner = Psych::ScalarScanner.new(Psych::ClassLoader.new)
       source = ::Vim.evaluate("getbufline(#{bufnr}, 1, '$')").join("\n")
-      tokens = Parser.new(source).parse.handler.tokens
-      ::Vim.command "let s:result = #{tokens.to_json}"
+      @source = source.force_encoding(Encoding::UTF_8).split("\n")
+      node = Psych::Parser.new(Psych::TreeBuilder.new).parse(source).handler.root
+      traverse(node)
+      ::Vim.command "let s:result = #{@tokens.to_json}"
     end
   end
 end
